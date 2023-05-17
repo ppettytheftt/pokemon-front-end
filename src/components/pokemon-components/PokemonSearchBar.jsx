5c71cabd990d1a2017a86310c7a6c43b15038ba5
ppettytheftt <jason.c.coffman23@gmail.com>
2023-05-16 21:35:18 -0500

    rendering data!!!!

--- src/components/pokemon-components/PokemonSearchBar.jsx
+++ src/components/pokemon-components/PokemonSearchBar.jsx
@@ -3,17 +3,22 @@ import { useState } from "react";
 
 export const PokemonSearchBar = () => {
   const [field, setField] = useState();
-  const [pokemonData, setPokemonData] = useState([]);
+  const [pokemonData, setPokemonData] = useState();
+  const [showResults, setShowResults] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
     hitApi(field);
+    setShowResults(true);
+    console.log(pokemonData);
   }
 
   async function hitApi(searchTerm) {
     await fetch("http://localhost:9090/pokemon/name/" + searchTerm)
       .then((response) => response.json())
-      .then((json) => console.log(json))
+      .then((data) => {
+        setPokemonData(data);
+      })
       .catch((error) => console.log(error));
   }
 
@@ -28,6 +33,20 @@ export const PokemonSearchBar = () => {
           onChange={(e) => setField(e.target.value)}
         />
       </form>
+      {pokemonData && (
+        <>
+          <ul>
+            <li key={pokemonData.id}>Id: {pokemonData.id}</li>
+            <li key={pokemonData.name}>Name: {pokemonData.name}</li>
+            <li key={pokemonData.height}>height: {pokemonData.height}</li>
+            <li key={pokemonData.weight}>weight: {pokemonData.weight}</li>
+            <li key={pokemonData.baseExperience}>
+              baseExperience: {pokemonData.baseExperience}
+            </li>
+            <li key={pokemonData.order}>order: {pokemonData.order}</li>
+          </ul>
+        </>
+      )}
     </>
   );
 };
