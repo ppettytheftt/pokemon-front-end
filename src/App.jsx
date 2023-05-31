90f280d9235d0fe1977aca1716543dcac1f6d026
ppettytheftt <jason.c.coffman23@gmail.com>
2023-05-31 14:37:42 -0500

    removes react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { QueryClientProvider, QueryClient } from "react-query";
 import "./App.css";
 import { HomePage } from "./components/Home";
 import { PokemonSearchBar } from "./components/pokemon-components/PokemonSearchBar";
 
-const queryClient = new QueryClient();
-
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/pokemon">Pokemon Search</Link>
-              </li>
-            </ul>
-          </nav>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/pokemon" element={<PokemonSearchBar />} />
-          </Routes>
-        </div>
-      </Router>
-    </QueryClientProvider>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/pokemon">Pokemon Search</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pokemon" element={<PokemonSearchBar />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
